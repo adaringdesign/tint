6075f3238ee51d291189ddb036a9490bcaf3ec58
adaringdesign <info@adaringdesign.nl>
2017-05-16 22:36:27 +0200

    Cleanup
    Removed “Events” and “Extend” modules as they weren’t needed

--- src/status-bar.js
+++ src/status-bar.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const extend = require('extend')
-const events = require('events')
+
 const electron = require('electron')
 const Positioner = require('electron-positioner')
 
--- src/web.js
+++ src/web.js
@@ -8,8 +8,6 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const ColorPicker = require('react-color')
 
-
-
 const Tint = React.createClass({
 
   displayName: 'Tint',
